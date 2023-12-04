b678fc04bf186c74652cb2c628cd42c7fc471ac9
Carlos Fdez. Llamas <hello@sirikon.me>
2023-12-04 20:58:47 +0100

    wip

--- src/main.zig
+++ src/main.zig
@@ -5,21 +5,45 @@ pub fn main() !void {
 }
 
 pub fn day_1() !void {
-    const input_a =
-        \\1abc2
-        \\pqr3stu8vwx
-        \\a1b2c3d4e5f
-        \\treb7uchet
-    ;
-
-    for (input_a) |char| {
-        std.debug.print("{d}\n", .{char});
-    }
+    std.debug.print("{s}\n", .{"--- Day 1 ---"});
+    try resolve_day_1(@embedFile("./inputs/day_1/example.txt"));
+    try resolve_day_1(@embedFile("./inputs/day_1/part1.txt"));
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+pub fn resolve_day_1(input: []const u8) !void {
+    var it = std.mem.split(u8, input, "\n");
+    var sum: i32 = 0;
+    while (it.next()) |line| {
+        var first: ?u8 = null;
+        var last: ?u8 = null;
+
+        var i: usize = 0;
+        while (i < line.len) {
+            const char = line[i];
+            if (char >= '0' and char <= '9') {
+                first = char;
+                break;
+            }
+            i += 1;
+        }
+
+        i = line.len - 1;
+        while (i >= 0) {
+            const char = line[i];
+            if (char >= '0' and char <= '9') {
+                last = char;
+                break;
+            }
+            i -= 1;
+        }
+
+        if (first == null or last == null) {
+            unreachable;
+        }
+
+        const values = [2]u8{ first.?, last.? };
+        const result = try std.fmt.parseInt(i32, &values, 10);
+        sum += result;
+    }
+    std.debug.print("{d}\n", .{sum});
 }
