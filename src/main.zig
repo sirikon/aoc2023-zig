f0bad34b64749b9dee2facb66e2b66d70d6fc1b0
Carlos Fdez. Llamas <hello@sirikon.me>
2023-12-04 17:54:43 +0100

    wip

--- src/main.zig
+++ src/main.zig
@@ -1,19 +1,11 @@
 const std = @import("std");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    try day_1();
+}
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
+pub fn day_1() !void {
+    std.debug.print("Hello Day 1\n", .{});
 }
 
 test "simple test" {
